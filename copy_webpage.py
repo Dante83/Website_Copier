8c65a844cfd05f0805a17f521953c2e6c3a6988d
Dante83 <david.relativitytheory@gmail.com>
2014-05-06 17:33:22 -0700

    Fixed broken links between pages by checking which level the current page was on and rewriting the links as relative links.

--- copy_webpage.py
+++ copy_webpage.py
@@ -3,16 +3,14 @@ import sys
 import os
 import urllib2
 import urlparse
-import codecs
 from bs4 import BeautifulSoup
 from resource_acquisition_manager import *
 
 url_list = []
 
-def recursive_webpage_cursor(url_cursor, file_path, root_replacement):
+def recursive_webpage_cursor(url_cursor, file_path, root_replacement, file_depth):
     #Record that the present web-page is being tracked
-    if url_cursor not in url_list:
-        url_list.append(url_cursor)
+    url_list.append(url_cursor)
 
     #Acquire http response from url
     request = urllib2.Request(url_cursor)
@@ -38,25 +36,37 @@ def recursive_webpage_cursor(url_cursor, file_path, root_replacement):
     else:
         file_loc = os.path.join(current_directory, (file_name + '.html') )
 
-    modified_soup = BeautifulSoup(html.decode('utf-8'))
+    modified_soup = BeautifulSoup(html)
 
     for link in modified_soup.find_all('a'):
-        if (root_path) in link['href']:
-            updated_link = link['href'].replace(root_path, root_replacement)
-        elif not link['href'].endswith('.html'):
-            updated_link = link['href'] + '.html'
+        original_link = link['href']
+
+        #Make sure that all absolute path files and the index file use the right
+        #web address
+        if 'http://' + root_path + '/' == link['href']:
+            depth_relative_link_slashes = '../' * file_depth
+            updated_link = depth_relative_link_slashes + 'index.html'
         else:
-            updated_link = link['href']
+            #Make sure that all non-index files point to html files
+            if not link['href'].endswith('.html'):
+                updated_link = link['href'] + '.html'
+            else:
+                updated_link = link['href']
+
+        #Snip the first forward slash off of relative hyperlinks
+        if updated_link.startswith('/'):
+            depth_relative_link_slashes = '../' * file_depth
+            updated_link = depth_relative_link_slashes + updated_link[1:]
+
+        link['href'] = updated_link
 
     #Check to make sure we're not modifying a link that's already inside
     #of the cloned url list, if so add this link, too.
-    if link['href'] not in url_list:
+    if original_link not in url_list:
         url_list.append(updated_link)
-        link['href'] = updated_link
-
         try:
-            html_file = codecs.open(file_loc, 'w', 'utf-8')
-            html_file.write( html.decode('utf-8'))
+            html_file = open(file_loc, 'w')
+            html_file.write( modified_soup.prettify('utf-8'))
         except IOError as e:
             print 'IO Write Error: %s'%e
             sys.exit(0)
@@ -86,11 +96,11 @@ def recursive_webpage_cursor(url_cursor, file_path, root_replacement):
     hyperlinks = filter(lambda link: '#' not in link['href'], hyperlinks)
 
     #Filter out external hyper-links (e.g. don't copy all of google.com)
-    hyperlinks = filter(lambda link: link['href'].startswith('/') or link['href'].startswith(url_components.netloc), hyperlinks)
+    hyperlinks = filter(lambda link: not link['href'].startswith('http://') or link['href'].startswith(url_components.netloc), hyperlinks)
 
     #Recursively go about traveling through each of these hyper-links repeating
     #this process on those pages that haven't already been created.
     for link in hyperlinks:
         next_page = urlparse.urljoin(url_cursor, link['href'])
         if next_page not in url_list:
-            recursive_webpage_cursor(next_page, file_path, root_replacement)
+            recursive_webpage_cursor(next_page, file_path, root_replacement, next_page.count('/')  - 3)
