7feb800a7568d05435f4bec73f75ae645722c9ff
Dante83 <david.relativitytheory@gmail.com>
2014-05-09 17:01:04 -0700

    ready to push to remote host

--- main.py
+++ main.py
@@ -4,21 +4,18 @@ import Tkinter, tkFileDialog
 from copy_webpage import *
 
 #Acquire the web-page url
-#url = raw_input("Enter the webpage base url to start from: ")
+url = raw_input("Enter the webpage base url to start from: ")
 
 #Acquire a replacement root for localhost or current web-page name
-#rep_root = raw_input("Enter the replacement webpage root: ")
+rep_root = raw_input("Enter the replacement webpage root: ")
 
 #Acquire the path for the output web-page
-#try:
-#    root = Tkinter.Tk()
-#    root.withdraw()
-#    file_path = tkFileDialog.askdirectory()
-#except:
-#    print 'TKinter Error - No File Directory Detected'
-
-#TODO: fix up the url so that it's in the right format, even if the user
-#enters a short-hand version.
+try:
+    root = Tkinter.Tk()
+    root.withdraw()
+    file_path = tkFileDialog.askdirectory()
+except:
+    print 'TKinter Error - No File Directory Detected'
 
 #Initialize the web-page recursion loop
-recursive_webpage_cursor('http://localhost:3000', '/Users/davidevans/Documents/Code_Panda_Proving_Grounds', 'code-panda.com', 0, '/Users/davidevans/Documents/Code_Panda_Proving_Grounds')
+recursive_webpage_cursor(url, file_path, rep_root, 0, file_path)
