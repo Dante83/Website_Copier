b22bc49514d6ac860096a2f88eadeafc08edbc6a
Dante83 <david.relativitytheory@gmail.com>
2014-05-22 10:52:09 -0700

    Forgot to set up the main area for handling regular pages again.

--- main.py
+++ main.py
@@ -4,21 +4,18 @@ import Tkinter, tkFileDialog
 from copy_webpage import *
 
 #Acquire the web-page url
-#url = raw_input("Enter the webpage base url to start from: ")
-url = 'http://localhost:3000/'
+url = raw_input("Enter the webpage base url to start from: ")
 
 #Acquire a replacement root for localhost or current web-page name
-#rep_root = raw_input("Enter the replacement webpage root: ")
-rep_root = 'http://www.code-panda.com/bhts/pages'
+rep_root = raw_input("Enter the replacement webpage root: ")
 
-file_path = '/Users/davidevans/Documents/Bhutan-Static-Page'
 #Acquire the path for the output web-page
-#try:
-#    root = Tkinter.Tk()
-#    root.withdraw()
-#    file_path = tkFileDialog.askdirectory()
-#except:
-#    print 'TKinter Error - No File Directory Detected'
+try:
+    root = Tkinter.Tk()
+    root.withdraw()
+    file_path = tkFileDialog.askdirectory()
+except:
+    print 'TKinter Error - No File Directory Detected'
 
 #Initialize the web-page recursion loop
 recursive_webpage_cursor(url, file_path, rep_root, 0, file_path)
